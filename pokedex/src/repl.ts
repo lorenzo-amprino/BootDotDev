3a207b2e63ac327a5b96e3d19fb814f936de519d
Lorenzo Amprino <lorenzo.amprino@beije.it>
2025-10-25 15:13:57 +0200

    commit iniziale

---
+++ pokedex/src/repl.ts
@@ -0,0 +1,26 @@
+import { createInterface } from "readline";
+
+export function cleanInput(input: string): string[] {
+  if(input.length == 0) return [];
+
+  const arr = input.trim().replaceAll(/\s+/g, " ").split(" ");
+  return arr.map((x) => x.toLowerCase());
+}
+
+export function startREPL(){
+    const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex ",
+});
+rl.prompt();
+rl.on("line", (input) =>{
+    const inputArr = cleanInput(input);
+    if(inputArr.length == 0) {
+        rl.prompt();
+    } else {
+        console.log(`Your command was: ${inputArr[0]}`);
+        rl.prompt();
+    }
+})
+}
