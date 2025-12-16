6dfaac1023e7c65669d6b1309518dfa00a37ca23
Lorenzo Amprino <lorenzo.amprino@beije.it>
2025-12-16 15:20:41 +0100

    avanzamento

--- rss_feed_aggregator/src/lib/db/queries/feeds.ts
+++ rss_feed_aggregator/src/lib/db/queries/feeds.ts
@@ -1,6 +1,6 @@
 import { db } from "..";
 import { feeds } from "../schema";
-import { eq } from "drizzle-orm";
+import { eq, InferSelectModel, sql} from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, user_id: string) {
   const [result] = await db.insert(feeds).values({ name: name, url: url, user_id: user_id }).returning();
@@ -18,6 +18,17 @@ export async function getFeed_by_url(url: string) {
 
 export async function deleteFeeds() {
   await db.delete(feeds);
+}
+
+export async function markFeedFetched(id: string){
+  await db.update(feeds).set({last_fetched_at: new Date(), updatedAt: new Date()}).where(eq(feeds.id, id));
+}
+
+export async function getNextFeedToFetch():Promise<Feed | undefined> {
+  const res = await db.execute(sql<Feed>`select * from ${feeds} order by ${feeds.last_fetched_at} asc null first limit 1;`);
+
+  const rows = res as unknown as Feed[];
+  return rows[0];
 }
 
 export type Feed = typeof feeds.$inferSelect;
